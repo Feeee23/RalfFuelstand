691f63a3993dffff28c5f36fbaedc4a604f41716
Feeee23 <63659321+Feeee23@users.noreply.github.com>
2020-11-12 15:57:46 +0100

    Update ZisterneRalf.ino

--- ZisterneRalf.ino
+++ ZisterneRalf.ino
@@ -1,25 +1,17 @@
 // Load Wi-Fi library
-#include <ESP8266WiFi.h> //WIFI-Lib
+#include <ESP8266WiFi.h>
 #include <SR04.h> //Ultarschallsensor
-#include <NTPClient.h> 
+#include <NTPClient.h>
 #include <WiFiUdp.h>
 #include <TimeLib.h>
-#include <EMailSender.h>
 
 // Replace with your network credentials
-const char* ssid     = "Paradieswerkstatt"; //"WLAN Ralf"
-const char* password = "k*H#96*c*Z#93"; //"44727291469489115864"
+const char* ssid     = "HansGustav38.2"; //"WLAN Ralf"
+const char* password = "Frieder17"; //"44727291469489115864"
 
-//Emailadresse und PW hinterlegen
-EMailSender emailSend("ralfszisterne@gmail.com", "A#dCa3D-;yky");
-const char* empfaenger = "felix.girke@web.de";
-EMailSender::EMailMessage messageVoll;
-EMailSender::EMailMessage messageLeer;
-EMailSender::EMailMessage messageAnfrage;
-
-WiFiUDP ntpUDP; //Fuer die Package´s die von den Zeit Server zu holen sind
-long utcOffsetInSeconds = 3600; // offset von UTF zeit (zur Witerzeit, zur Sommerzeit erhöhen auf 7200)
-NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds); //Adresse und Client für die Zeit desfinieren
+WiFiUDP ntpUDP;
+long utcOffsetInSeconds = 7200;
+NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
 
 // Set web server port number to 80
 WiFiServer server(80);
@@ -28,17 +20,17 @@ WiFiServer server(80);
 String header;
 
 // Assign output variables to GPIO pins
+const int testpin = 15;
 String s="";
-const int historie=50; //Wieviel Werte sollen gespeichert werden
-int a[historie]; //Speicherplatz für die Abstände
-long long t[historie]; //Speicherplatz für die Zeitpunkte
+const int historie=50;
+int a[historie];
+long long t[historie];
 int Trig=5; //Entspricht D1
 int Echo=4; //Entspricht D2
-int i=0; //zaehler Schleife ausgabe
-int k=0; //zaehler einmal taegliche abfrage
-int j=0; //offset Sommer/Winterzeit
-int cont=0;
+int i=0;
+int k=0;
 SR04 sr04= SR04(Trig, Echo);
+
 
 // Current time
 unsigned long currentTime = millis();
@@ -49,6 +41,11 @@ const long timeoutTime = 2000;
 
 void setup() {
   Serial.begin(115200);
+  // Initialize the output variables as outputs
+  pinMode(testpin, OUTPUT);
+  // Set outputs to LOW
+  digitalWrite(testpin, LOW);
+
   // Connect to Wi-Fi network with SSID and password
   Serial.print("Connecting to ");
   Serial.println(ssid);
@@ -64,15 +61,6 @@ void setup() {
   Serial.println(WiFi.localIP());
   server.begin();
   timeClient.begin();
-
-  ///////////////////////////////////////////Email Nachrichten festlegen
-  messageVoll.subject="Zisterne Voll";
-  messageVoll.message="Die Zisterne ist ueber 200cm voll";
-  messageLeer.subject="Zisterne Leer";
-  messageLeer.message="Die Zisterne ist nur noch unter 50cm voll";
-  messageAnfrage.subject="Service Anfrage";
-  messageAnfrage.message="Der Server ist online und Email funktioniert.";
-  
 }
 ////////////////////////////////////////////////////Sensor
 int Sensor(){
@@ -80,24 +68,17 @@ int Sensor(){
   int B=sr04.Distance();
   int C=sr04.Distance();
   int D=(A+B+C)/3; //Durchschnitt der 3 Werte ermitteln
-  Serial.print(D); Serial.println("cm");
-  int F=240-D; //Die Höhe des Sensors (240cm) minus den Messwert ergibt den Füllstand
+  int F=D; //Die Höhe des Sensors (190cm) minus den Messwert ergibt den Füllstand
   return F;
   }
 ////////////////////////////////////////////////////////////Rechner
 int Rechner(int a){
-   float k=0.0; //definition rueckgage variable als float zum max 1. liter rundungsfehler zu haben
-   float f=(float)a/10; //Fuelstand in dezimeter und in als float
-   if(a<200){
-    k=22*(100*acos(1-(f/10))-(10-f)*sqrt((20*f)-(f*f))); //macht aus der Füllhöhe die Liter (liegender Zylinder) Quelle:https://www.scalesoft.de/math/tank/tank.html
-   }else{
-    k=6283+55.416*(f-20); //Uber dem Zylinder
-   }
-   return (int)k; //rueckgabe der wertes mit typecast zu int
+   int k=2*a; //macht aus der Füllhöhe die Liter
+  return k;
   }
   ////////////////////////////////////////////////////////////////Umspeichern
-void Umspeichern(int* a, long long* t){ //Speicher die Daten im Array um das nur die neueste Gespeichert sind
-  for(i=(historie-1);i>=0;i--){ //von hinten nimm das 2. letzte legs an letzte Pos. usw.
+void Umspeichern(int* a, long long* t){ //Speicher die Daten um das nur die neueste Gespeichert sind
+  for(i=(historie-1);i>=0;i--){
     a[i]=a[i-1];
     t[i]=t[i-1];
   }
@@ -105,24 +86,18 @@ void Umspeichern(int* a, long long* t){ //Speicher die Daten im Array um das nur
 }
   ////////////////////////////////////////////////////////////////loop
 void loop(){
-  timeClient.update(); 
-  if (hour(timeClient.getEpochTime()+j)==2 & k==0){//einmal Täglich abrufen um 12:00Uhr
-    Umspeichern(&a[0], &t[0]); 
+  timeClient.update(); //einmal Täglich abrufen um 12:00Uhr
+  if (hour(timeClient.getEpochTime())==12 & k==0){//3:14:15
+    Umspeichern(&a[0], &t[0]);
     a[0]=Sensor();
     timeClient.update();
-    t[0]=(timeClient.getEpochTime()+j);
+    t[0]=timeClient.getEpochTime();
     k++;
-    if (a[0]<50){//wenn Fuellstand zu niedrig -> Email
-      EMailSender::Response resp=emailSend.send(empfaenger, messageLeer);
-    }
-    if (a[0]>200){//wenn Fuellstand zu hoch -> Email
-      EMailSender::Response resp=emailSend.send(empfaenger, messageVoll);
-    }
   }
-  if(hour(timeClient.getEpochTime()+j)==3){ //Zaehler für einmal tägliches Abrufen zurücksetzten
+  if(hour(timeClient.getEpochTime())==21){
     k==0;
   }
-  
+
   WiFiClient client = server.available();   // Listen for incoming clients
 
   if (client) {                             // If a new client connects,
@@ -130,7 +105,7 @@ void loop(){
     String currentLine = "";                // make a String to hold incoming data from the client
     currentTime = millis();
     previousTime = currentTime;
-    while (client.connected() && currentTime - previousTime <= timeoutTime) { // loop while the client's connected and not timed out
+    while (client.connected() && currentTime - previousTime <= timeoutTime) { // loop while the client's connected
       currentTime = millis();         
       if (client.available()) {             // if there's bytes to read from the client,
         char c = client.read();             // read a byte, then
@@ -147,84 +122,39 @@ void loop(){
             client.println("Content-type:text/html");
             client.println("Connection: close");
             client.println();
-            if (header.indexOf("GET /6/on") >= 0) { //Wenn Button aktiv -> Sommerzeit
-               j=3600;
-            }else if(header.indexOf("GET /6/off") >= 0){
-              j=0;
-            }
-            if((header.indexOf("GET /7/on") >= 0)&&cont==0) {//Wenn Button aktiv -> Email senden
-              cont++; //Das es nicht dauernt abgeht, wird erst durch aktivieren von/5/on wieder auf Null gesetzt
-              EMailSender::Response resp=emailSend.send(empfaenger, messageAnfrage);
-            }
-            // "Wartet" auf den Aufruf der ip/5/on Seite
+            // turns the GPIOs on and off
             if (header.indexOf("GET /5/on") >= 0) {
-              cont=0;
+              Serial.println("GPIO 5 on");
+              //digitalWrite(testpin, HIGH);
               Umspeichern(&a[0], &t[0]);
-              a[0]=Sensor(); //Speichert den Abstand
+              a[0]=Sensor();
               timeClient.update();
-              t[0]=(timeClient.getEpochTime()+j); //Speichert die dazugehörige Zeit (UNIX) evtl. plus 1h fuer Sommerzeit
+              t[0]=timeClient.getEpochTime();
+              //digitalWrite(testpin, LOW);
             } 
-            //Start HTML Seite, Kommentarte sind dort
-            client.println("<!DOCTYPE html><html lang='de'>");
+            client.println("<!DOCTYPE html><html>");
             client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-            client.println("<meta name='author' content='Felix Girke'>");
-            client.println("<meta name='viewport' content='width=device-width, inital-scale=1.0'>");
             client.println("<link rel=\"icon\" href=\"data:,\">");
-            // Graph (javascript) von https://developers-dot-devsite-v2-prod.appspot.com/chart/interactive/docs/gallery/linechart.html#overview
-            client.println("<script type='text/javascript' src='https://www.gstatic.com/charts/loader.js'></script><script type='text/javascript'>google.charts.load('current', {'packages':['corechart']});google.charts.setOnLoadCallback(drawChart)");
-            client.println("function drawChart(){var data = google.visualization.arrayToDataTable([['Datum', 'Liter']");
-             for(i=0; i<historie; i++){ //Mach tabelleneinträge für alle einträge (histoire)
-            if(t[i]==0){ //Wenn kein wert mehr drin steht, brich ab
-              break;
-            }
-            client.print(",['");
-            if (day(t[i])<10){
-              client.print("0");
-            }
-            client.print(day(t[i]));
-            client.print(".");
-            if(month(t[i])<10){
-              client.print("0");
-            }
-            client.print(month(t[i]));
-            client.print(".");
-            client.print(year(t[i]));
-            client.print("',");
-            client.print(Rechner(a[i]));
-            client.println("]");
-            //,['19.09.2020',200],['20.09.2020',200],['21.09.2020',660],['22.09.2020',400],['23.09.2020',199]
-             }
-            client.println("]);var options={curveType:'none',legend:'none',");
-            client.println("};var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));chart.draw(data, options);}</script>");
             // CSS to style the on/off buttons 
             // Feel free to change the background-color and font-size attributes to fit your preferences
             client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
-            client.println(".button {background-color: #00008B; border: none; color: white; padding: 16px 40px; text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
-            client.println("table, th, td  {border: 1px solid black; border-collapse: collapse;}</style> <title>Ralf's Zisternen Fuellstand</title></head>");
-            
-            // Web Page Body
-            client.println("<body><h1>Ralf's Zisternen Fuellstand</h1>");
-            if (j==3600) {
-              client.println("<p>Die Uhrzeit wird aktuell in Sommerzeit angezeigt</p>");
-            }else{
-             client.println("<p>Die Uhrzeit wird aktuell in Winterzeit angezeigt</p>");
-            }
+            client.println(".button {background-color: #195B6A; border: none; color: white; padding: 16px 40px; text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            //client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            client.println("table, th, td  {border: 1px solid black; border-collapse: collapse;}</style></head>");
+
+            // Web Page Heading
+            client.println("<body><h1>Ralfs Zisternen Fuellstand</h1>");
             client.println("<p>neue Abfrage:</p>");
-            client.println("<p><a href=\"/5/on\"><button class=\"button\">Start</button></a><a href=\'/'><button class=\"button\" style='background-color:#228B22'>aktualisieren</button></a></p>");
-            if (j==3600) {
-              client.println("<p><a href=\"/6/off\"><button class=\"button\" style='background-color:#3399ff'>Winterzeit aktivieren</button></a></p>");
-            }else{
-             client.println("<p><a href=\"/6/on\"><button class=\"button\" style='background-color:#ffb84d'>Sommerzeit aktivieren</button></a></p>"); 
-            }
+            client.println("<p><a href=\"/5/on\"><button class=\"button\">Start</button></a><a href=\'/'><button class=\"button\">aktualisieren</button></a></p>");
             client.print("<p>Der letzte gemessene Fuellstand sind<b> ");
             client.print(a[0]);
             client.println("cm</b></p>");
             client.print("<p>Dies entspricht ungefaehr<b> ");
             client.print(Rechner(a[0]));
-            client.println("Liter</b></p><div style=' width: auto; height: 500px' id='curve_chart'></div> <br><table style='width:100%'>");
+            client.println("Liter</b></p> <br><table style='width:100%'>");
             client.println("<tr><th style='border-top:hidden;border-bottom:hidden;'></th><th style='width:150px'>Datum</th><th style='width:100px'>Fuellhoehe</th><th style='width:100px'>Liter</th><th style='border-top:hidden;border-bottom:hidden;border-right:hidden;'></th></tr>");
-           for(i=0; i<historie; i++){ //Mach tabelleneinträge für alle einträge (histoire)
-            if(t[i]==0){ //Wenn kein wert mehr drin steht, brich ab
+           for(i=0; i<historie; i++){
+            if(t[i]==0){
               break;
             }
             client.print("<tr><td style='border-top:hidden;border-bottom:hidden;border-left:hidden;'> </td><td>");
@@ -255,8 +185,6 @@ void loop(){
             client.print(Rechner(a[i]));
             client.println("l</td><td style='border-top:hidden;border-bottom:hidden;border-right:hidden;'> </td></tr>");
            }
-           
-            client.println("</table><a href='/7/on'><img src='https://tse1.mm.bing.net/th?id=OIP._h5ptKPwlk6jR6MdVHmukwHaHa&pid=Api' alt='Email' align='left' style='width:50px;height:50px;'></a><p style='text-align:right'>Die aktuelle Version 1.2 des Quellcodes gibt es <a href='https://github.com/Feeee23/RalfFuelstand/tree/1.2mitEmail' target='_blank' >hier</a></p>");
             client.println("</body></html>");
 
             // The HTTP response ends with another blank line
@@ -280,10 +208,12 @@ void loop(){
   }
 }
 
-/* ToDo:
- * Email Abfrage verbesser evtl. andere Programiersprachen 
- * 
- * 
+/* ToDo
+ * rechner richten
+ * Diagram zeichnen
+ * stecker
+ * verteilerdose
  * historie verlängern?
  * Bilder
+ * sommerzeit
  */
